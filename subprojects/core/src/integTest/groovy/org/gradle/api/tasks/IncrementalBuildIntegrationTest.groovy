4e208e8901a5308ffb7f278f14808bc64483e139
Adam Murdoch <adam@gradle.com>
2017-01-13 15:34:49 +1000

    Removed the guarantee that changes to output files made immediately before build are detected, as this requires always rehashing the build outputs produced during the first timestamp period of the previous build even though they are very unlikely to have changed.
    Changes to input files are still detected, as these are more likely to have changed.

--- subprojects/core/src/integTest/groovy/org/gradle/api/tasks/IncrementalBuildIntegrationTest.groovy
+++ subprojects/core/src/integTest/groovy/org/gradle/api/tasks/IncrementalBuildIntegrationTest.groovy
@@ -228,24 +228,6 @@ apply from: 'changes.gradle'
         then:
         skippedTasks.sort() == [":a", ":b"]
 
-        // Change intermediate output file, same length
-        when:
-        outputFileA.text = '[NEW CONTENT]'
-        succeeds "b"
-
-        then:
-        nonSkippedTasks.sort() ==  [":a"]
-        skippedTasks.sort() ==  [":b"]
-
-        outputFileA.text == '[new content]'
-        outputFileB.text == '[[new content]]'
-
-        when:
-        succeeds "b"
-
-        then:
-        skippedTasks.sort() == [":a", ":b"]
-
         // Change intermediate output file, different length
         when:
         outputFileA.text = 'changed'
@@ -264,7 +246,7 @@ apply from: 'changes.gradle'
         then:
         skippedTasks.sort() == [":a", ":b"]
 
-        // Change intermediate output file timestamp
+        // Change intermediate output file timestamp, same content
         when:
         outputFileA.makeOlder()
         succeeds "b"
@@ -567,7 +549,7 @@ task b(type: DirTransformerTask, dependsOn: a) {
         skippedTasks.sort() ==  [":a", ":b"]
     }
 
-    def "notices changes to input and output files where the file length does not change"() {
+    def "notices changes to input files where the file length does not change"() {
         writeTransformerTask()
         writeDirTransformerTask()
 
@@ -584,26 +566,16 @@ task b(type: DirTransformerTask, dependsOn: a) {
 
         given:
         def inputFile = file('src.txt')
-        def outputFile = file('build/a/src.txt')
         inputFile.text = "__"
+        int before = inputFile.length()
 
         expect:
         (10..40).each {
-            int before = inputFile.length()
             inputFile.text = it as String
             assert inputFile.length() == before
 
             succeeds("b")
             result.assertTasksNotSkipped(":a", ":b")
-        }
-
-        (10..40).each {
-            int before = outputFile.length()
-            outputFile.text = outputFile.text.replaceAll("\\d", "_")
-            assert outputFile.length() == before
-
-            succeeds("b")
-            result.assertTasksNotSkipped(":a")
         }
     }
 
